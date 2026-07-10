368ecd324ad43a7627b0decd981347b819d979b3
galko121228-design <galko121228@gmail.com>
2026-07-11 03:13:51 +0500

    true fullscreen no insets

--- app/src/main/java/org/nobiam/MainActivity.java
+++ app/src/main/java/org/nobiam/MainActivity.java
@@ -1,23 +1,36 @@
 package org.nobiam;
 
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // ПОЛНЫЙ fullscreen без inset'ов
-        Window window = getWindow();
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        );
-
         setContentView(R.layout.activity_main);
+
+        // УБИРАЕМ ВСЕ системные отступы
+        View decorView = getWindow().getDecorView();
+
+        WindowInsetsControllerCompat controller =
+                ViewCompat.getWindowInsetsController(decorView);
+
+        if (controller != null) {
+            controller.hide(WindowInsetsCompat.Type.systemBars());
+            controller.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            );
+        }
+
+        // УБИРАЕМ inset padding вообще
+        ViewCompat.setOnApplyWindowInsetsListener(decorView, (v, insets) -> {
+            return WindowInsetsCompat.CONSUMED;
+        });
     }
 }
