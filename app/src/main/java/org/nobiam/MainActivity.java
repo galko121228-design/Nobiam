21549b21a15587590f4f412dac0b01cf9102e019
galko121228-design <galko121228@gmail.com>
2026-07-11 02:15:51 +0500

    IMMERSIVE FULLSCREEN (no status bar, no nav bar)

--- app/src/main/java/org/nobiam/MainActivity.java
+++ app/src/main/java/org/nobiam/MainActivity.java
@@ -16,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+getWindow().getDecorView().setSystemUiVisibility(
+    android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY 
+    | android.view.View.SYSTEM_UI_FLAG_FULLSCREEN 
+    | android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION 
+    | android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN 
+    | android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION 
+    | android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+);
 
         navHome = findViewById(R.id.navHome);
         navSettings = findViewById(R.id.navSettings);
