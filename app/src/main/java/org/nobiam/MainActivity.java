2b19b6ad99bbed4ac04084e092e71bc6d2adcc78
galko121228-design <galko121228@gmail.com>
2026-07-11 02:45:13 +0500

    fix fullscreen + ui

--- app/src/main/java/org/nobiam/MainActivity.java
+++ app/src/main/java/org/nobiam/MainActivity.java
@@ -1,12 +1,24 @@
 package org.nobiam;
 
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // УБИРАЕМ ВСЁ (status + nav)
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
 
         setContentView(R.layout.activity_main);
     }
