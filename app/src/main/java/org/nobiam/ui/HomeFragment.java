22994537b6674b92456d23af6b872f5ddc2466df
galko121228-design <galko121228@gmail.com>
2026-07-11 20:20:46 +0500

    Add GlowButton library for real neon glow

--- app/src/main/java/org/nobiam/ui/HomeFragment.java
+++ app/src/main/java/org/nobiam/ui/HomeFragment.java
@@ -4,9 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.Toast;
 import androidx.fragment.app.Fragment;
+import com.smb.glowbutton.GlowButton;
 import org.nobiam.R;
 
 public class HomeFragment extends Fragment {
@@ -15,7 +15,7 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        ImageButton btnPlay = view.findViewById(R.id.btnPlay);
+        GlowButton btnPlay = view.findViewById(R.id.btnPlay);
         btnPlay.setOnClickListener(v -> {
             Toast.makeText(getContext(), "Launching Minecraft...", Toast.LENGTH_SHORT).show();
         });
