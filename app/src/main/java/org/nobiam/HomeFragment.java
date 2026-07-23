5a5a9bdc745bc11ce93a7316d77290f4a2382a65
galko121228-design <galko121228@gmail.com>
2026-07-23 22:03:07 +0500

    Update HomeFragment.java

--- app/src/main/java/org/nobiam/HomeFragment.java
+++ app/src/main/java/org/nobiam/HomeFragment.java
@@ -1,15 +1,39 @@
 package org.nobiam;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.util.Log;
+import android.widget.Button;
+
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button playButton = view.findViewById(R.id.play_button); // ВАЖНО
+
+        if (playButton != null) {
+            playButton.setOnClickListener(v -> {
+                Log.d("NOBIAM", "Кнопка нажата");
+
+                Intent intent = new Intent();
+                intent.setClassName(
+                        "com.mojang.minecraftpe",
+                        "com.mojang.minecraftpe.MainActivity"
+                );
+
+                startActivity(intent);
+            });
+        }
+
+        return view;
     }
 }
